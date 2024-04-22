d6ee89d67bfaa811682b2508b4e4e92afa8660e4
Mark Gilbert <mgilbert@esri.com>
2024-04-22 14:45:21 -0700

    Updated header and paths

--- update_FC_field_names.py
+++ update_FC_field_names.py
@@ -1,24 +1,29 @@
 """
+-------------------------------------------------------------------------------
 File: update_FC_field_names.py
 Author: Mark E. Gilbert
 Date: 2024-04-16: Converted from Notebook to srcipt file.
 Description: This script is used to update the original feature class attribute names to something shorter
 since publishing using the Python API does not support the larger character length names. The attribute 
 alias and other properties get updated using Lisa B's Python script in a later step.
+
+TODOs:
+- Update path_to_file for the data file
+- Update path_to_workspace for the workspace
+
 """
 
 # Imports
+#
 import arcpy
 import pandas as pd
-import time
 
 # Constants & Workspace
 IN_FC_NAME = "ag_pivot_2022_county_sel_cmdty"
     
-SRC_NAMES = r"C:\Users\mark9020\OneDrive - Esri\Documents\Projects\Census of Agriculture/" \
-            r"Update Feature Class\UPDATE_ag_pivot_2022_county_sel_cmdty_FC.xlsx"
+SRC_NAMES = r"C:\path_to_file\UPDATE_ag_pivot_2022_county_sel_cmdty_FC.xlsx"
 
-arcpy.env.workspace = r"C:\Users\mark9020\Documents\ArcGIS\Projects\Ag Census\Ag Census 2022.gdb"
+arcpy.env.workspace = r"C:\path_to_workspace\Ag Census 2022.gdb"
 
 
 def main():
